2085fd841d25dcb6c390117c3f70a0dd857d6135
Nesho Neshev <neshev1978@gmail.com>
2022-02-17 09:05:09 +0200

    Add files via upload

---
+++ TwoWaySort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Partition
+{
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var tws = new Tws();
+            
+
+            tws.SortTwoWay(arr, arr.Length);
+
+            Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+
+
+}
---
+++ TwoWaySort/Tws.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+namespace Partition
+{
+    public class Tws
+    {
+
+        public void SortTwoWay(int[] arr, int n)
+        {
+
+            int leftIndex = 0, rightIndex = n - 1;
+
+            int count = 0;
+
+            while (leftIndex < rightIndex)
+            {
+                while (arr[leftIndex] % 2 != 0)
+                {
+                    leftIndex++;
+                    count++;
+                }
+
+                while (arr[rightIndex] % 2 == 0 && leftIndex < rightIndex)
+                    rightIndex--;
+
+                if (leftIndex < rightIndex)
+                {
+
+                    int temp = arr[leftIndex];
+                    arr[leftIndex] = arr[rightIndex];
+                    arr[rightIndex] = temp;
+                }
+            }
+
+            Array.Sort(arr, 0, count);
+            Array.Reverse(arr, 0, count);
+
+            Array.Sort(arr, count, n - count);
+        }
+    }
+}
